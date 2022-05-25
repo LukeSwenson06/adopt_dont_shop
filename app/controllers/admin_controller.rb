e305aba953db238c3717aafe4ec964d91724a4dc
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-25 17:35:30 -0600

    in-progress

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,4 +8,15 @@ class AdminController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def update
+    application = Application.find(params[:id])
+    @application_pet = ApplicationPet.find_by(application_id: params[:id], pet_id: params[:pet_id])
+      if params[:status] == "Approved"
+        @application_pet.update(status: 'Approved')
+      else
+        @application_pet.update(status: 'Rejected')
+      end
+    redirect_to "/admin/applications/#{@application.id}"
+  end
 end
